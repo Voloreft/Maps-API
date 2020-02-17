39b4a2f007d325afe3b735dc0edd510e9d4aeb43
MonitorPC <48449929+MonitorPC@users.noreply.github.com>
2020-02-17 11:27:46 +0300

    Update 1zadacha.py

--- 1zadacha.py
+++ 1zadacha.py
@@ -2,6 +2,7 @@ import requests
 import pygame
 
 coords = '52.299427,54.898788' # Альметьевск
+spns = '0.005,0.005'
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
@@ -21,6 +22,30 @@ toponym = res_j["response"]["GeoObjectCollection"][
 
 coords_toponym = toponym["Point"]["pos"]
 
-pygame.init()
+map_params = {
+    "ll": ",".join(coords_toponym.split(' ')),
+    "spn": spns,
+    "l": "map"
+}
 
-screen = pygame.display.set_mod()
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(map_api_server, params=map_params)
+
+photo = 'p.png'
+with open(photo, 'wb') as f:
+    f.write(response.content)
+
+
+pygame.init()
+size = (800, 800)
+screen = pygame.display.set_mod(size)
+image = pygame.image.load(photo).convert()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    screen.blit(image, (0, 0))
+    pygame.display.flip()
+pygame.quit()
